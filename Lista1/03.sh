9de6bee3b4ca6b0c6a1219c2c34df11de7bebac2
Victor Santos <61319771+aokami-aka@users.noreply.github.com>
2021-07-30 11:17:20 -0300

    Add files via upload

---
+++ Lista1/03.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "-------- DIRETÓRIO 1 --------"
+ls $1
+
+echo -e "\n-------- DIRETÓRIO 2 --------"
+ls $2
+
+echo -e "\n-------- DIRETÓRIO 3 --------"
+ls $3
+
+echo -e "\n-------- DIRETÓRIO 4 --------"
+ls $4
+
+echo -e "\n-------- DIRETÓRIO 5 --------"
+ls $5
